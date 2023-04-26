9dba06fb3175a0c4cf9517873f72a88610de4344
Farul Wananda <farulahmad2002@gmail.com>
2023-04-26 23:31:15 +0700

    update: modules version v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/farulwananda/go-lab-app
 
 go 1.20
 
-require github.com/farulwananda/go-lab-modules v1.2.0
+require github.com/farulwananda/go-lab-modules/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	go_lab_modules "github.com/farulwananda/go-lab-modules"
+	go_lab_modules "github.com/farulwananda/go-lab-modules/v2"
 )
 
 func main() {
-	fmt.Println(go_lab_modules.HelloWorld())
+	fmt.Println(go_lab_modules.HelloWorld("Farul"))
 }
