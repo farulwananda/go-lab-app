00cf6c169036d9284590d6d90f8d07ff48a48b89
Farul Wananda <farulahmad2002@gmail.com>
2023-04-26 23:17:03 +0700

    go app initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/farulwananda/go-lab-app
+
+go 1.20
+
+require github.com/farulwananda/go-lab-modules v1.2.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_lab_modules "github.com/farulwananda/go-lab-modules"
+)
+
+func main() {
+	fmt.Println(go_lab_modules.HelloWorld())
+}
